0c57a6dc651d7b027989ea2676cd0beb083297b7
Benny93 <vollmerbenjamin93@gmail.com>
2017-06-15 23:30:12 +0200

    Added Dockerfile

--- create_redis_cluster.sh
+++ create_redis_cluster.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-docker run --name redis1 -d redis
+APP_NAME='redis1'
+IMAGE='redis/benny'
+
+docker stop ${APP_NAME}
+docker rm -f ${APP_NAME}
+
+docker run --name ${APP_NAME} -p 6379:6379 -p 16379:16379 -d ${IMAGE}
--- spy.py
+++ spy.py
@@ -2,8 +2,9 @@
 import redis
 import time
 
+# 'host': '172.17.0.2'
 config = {
-    'host': '172.17.0.2'
+    'host': '10.0.0.1'
 
 }
 
