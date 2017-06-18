f19df6b790825f95f6281fdb8d65b371a435c32a
Benny93 <vollmerbenjamin93@gmail.com>
2017-06-18 22:29:36 +0200

    Changed Redis to RedisCluster to support redis cluster

--- redis_vstate.py
+++ redis_vstate.py
@@ -2,15 +2,46 @@
 Virtual State for SDN implemented with REDIS
 
 """
-import redis
+import rediscluster
 
 
 class State(object):
-    config = {
-        'host': '10.0.0.1',
-        'port': '7002'
+    OBJECT_IDENTIFIER = 'switch'
+    FIELD_IDENTIFIER = 'mac_to_port'
+
+    cluster = {
+        # node names
+        'nodes': {  # masters
+            'node_1': {'host': '10.0.0.1', 'port': 7001},
+            'node_2': {'host': '10.0.0.1', 'port': 7002},
+            'node_2': {'host': '10.0.0.1', 'port': 7003},
+        }
     }
 
     def __init__(self):
         # connect to session
-        self.redis = redis.StrictRedis(**self.config)
+        self.redis =\
+            rediscluster.StrictRedisCluster(cluster=self.cluster, db=0)
+
+    def get_mac_to_port(self, dpid):
+        """
+        Returns a the mac to port dictionary for switch with
+        :param dpid: datapath id of the switch
+        :return: dictionary
+        """
+        name = "{}:{}:{}".format(self.OBJECT_IDENTIFIER, dpid, self.FIELD_IDENTIFIER)
+        # if there is no entry for this name, return empty dict
+        if not self.redis.exists(name):
+            return {}
+        # else return the whole dictionary
+        return self.redis.hgetall(name)
+
+    def update_mac_to_port(self, dpid, data):
+        """
+
+        :param dpid: datapath id of the switch
+        :param data: updated dictionary
+        :return: dictionary
+        """
+        name = "{}:{}:{}".format(self.OBJECT_IDENTIFIER, dpid, self.FIELD_IDENTIFIER)
+        self.redis.hmset(name, data)
---
+++ test_state.py
@@ -0,0 +1,22 @@
+import unittest
+from unittest import TestCase
+
+
+class TestState(TestCase):
+    def test_get_mac_to_port(self):
+        import redis_vstate as rvs
+        state = rvs.State()
+        dpid = 123456789012340
+        my_dict = state.get_mac_to_port(dpid)
+        my_dict['00:00:00:00:00:00'] = 2
+        state.update_mac_to_port(dpid, my_dict)
+        my_dict = state.get_mac_to_port(dpid)
+        self.assertDictContainsSubset({'00:00:00:00:00:00': 2}, my_dict)
+
+    def test_update_mac_to_port(self):
+        self.fail()
+
+
+# Running Tests
+if __name__ == '__main__':
+    unittest.main()
