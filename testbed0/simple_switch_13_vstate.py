59f0cf7352242ee421b025eca0a34e19f60341ab
Benny93 <vollmerbenjamin93@gmail.com>
2017-06-20 11:13:24 +0200

    Added vstate learning switch

---
+++ testbed0/simple_switch_13_vstate.py
@@ -0,0 +1,76 @@
+from ryu.app import simple_switch_13
+from ryu.controller import ofp_event
+from ryu.controller.handler import set_ev_cls, MAIN_DISPATCHER
+from ryu.lib.packet import ether_types
+from ryu.lib.packet import ethernet
+from ryu.lib.packet import packet
+from virtual_state import redis_vstate as rvs
+
+
+class SimpleSwitch13VState(simple_switch_13.SimpleSwitch13):
+    """
+    This Class implements a simple learning switch, which stores its dictionaries
+    inside a virtual state data base.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(SimpleSwitch13VState, self).__init__(*args, **kwargs)
+        self.state = rvs.State()
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def _packet_in_handler(self, ev):
+        # If you hit this you might want to increase
+        # the "miss_send_length" of your switch
+        if ev.msg.msg_len < ev.msg.total_len:
+            self.logger.debug("packet truncated: only %s of %s bytes",
+                              ev.msg.msg_len, ev.msg.total_len)
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']
+
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocols(ethernet.ethernet)[0]
+
+        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
+            # ignore lldp packet
+            return
+        dst = eth.dst
+        src = eth.src
+
+        dpid = datapath.id
+        # Access DB
+        mac_to_port = self.state.get_mac_to_port(dpid)
+
+        self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
+
+        # learn a mac address to avoid FLOOD next time.
+        mac_to_port[src] = in_port
+
+        if dst in mac_to_port:
+            out_port = mac_to_port[dst]
+        else:
+            out_port = ofproto.OFPP_FLOOD
+        self.state.update_mac_to_port(dpid, mac_to_port)
+        # End of DB Access
+
+        actions = [parser.OFPActionOutput(out_port)]
+
+        # install a flow to avoid packet_in next time
+        if out_port != ofproto.OFPP_FLOOD:
+            match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
+            # verify if we have a valid buffer_id, if yes avoid to send both
+            # flow_mod & packet_out
+            if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+                self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+                return
+            else:
+                self.add_flow(datapath, 1, match, actions)
+        data = None
+        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+            data = msg.data
+
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                  in_port=in_port, actions=actions, data=data)
+        datapath.send_msg(out)
